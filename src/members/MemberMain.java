af75606f182e4086c738744c5663f11f426acff2
samsung99 <samsung99@DESKTOP-MH6J0QI>
2020-10-04 00:38:03 +0900

    members

--- src/members/MemberMain.java
+++ src/members/MemberMain.java
@@ -1,12 +1,15 @@
 package members;
+
+import members.data.Member;
+
 //회원관리의 기본을 표현
 //회원등록 ,회원리스트,로그인
 //회원 Member 스키마 => 기본데이터클래스
 //회원 객체들을 관리하는 매니져 클래스
 public class MemberMain {
-
+ 
 	public static void main(String[] args) {
-		
+		Member mb = new Member();
 		
 	}
 
