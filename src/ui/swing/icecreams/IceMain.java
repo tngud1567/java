cae75465dd33228c64f4a45a970eeb638b38acf1
samsung99 <samsung99@DESKTOP-MH6J0QI>
2020-10-06 01:38:14 +0900

    Icecream

---
+++ src/ui/swing/icecreams/IceMain.java
@@ -0,0 +1,10 @@
+package ui.swing.icecreams;
+
+public class IceMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ui/swing/icecreams/IcecreamStoreFrame.java
@@ -0,0 +1,7 @@
+package ui.swing.icecreams;
+
+import javax.swing.JFrame;
+
+public class IcecreamStoreFrame extends JFrame {
+
+}
